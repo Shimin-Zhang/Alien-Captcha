e4b5e5eb06b21f9c36049821988e4aa103ea09c0
John Zhang <john.shimin.zhang@gmail.com>
2014-11-24 01:00:44 -0500

    add some basic integration tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,25 +1,57 @@
 require "./spec/spec_helper"
 require "json"
 
+
 describe 'The Word Counting App' do
-  def app
-    Sinatra::Application
-  end
+  uri = URI('http://localhost:8000')
 
   it "returns 200 and has the right keys" do
-    get '/'
-    expect(last_response).to be_ok
-    parsed_response = JSON.parse(last_response.body)
-    expect(parsed_response).to have_key("text")
-    expect(parsed_response).to have_key("exclude")
+    response = Net::HTTP.get_response(uri)
+    expect(response.code).to eq("200")
+    expect(response.message).to eq("OK")
+    body = JSON.parse(response.body)
+    expect(body.keys).to include('sentence')
+    expect(body.keys).to include('exclude')
   end
 
   it "sends reponse 200 if the correct response is submitted" do
+    response = Net::HTTP.get_response(uri)
+    answer = get_answer(response)
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      new_response = http.request_post('/', answer.to_json)
+      expect(new_response.code).to eq("200")
+      expect(new_response.message).to eq("OK")
+    end
 
   end
 
-  it "sends 400 if incorrect numbers are submitted" do
+  it "sends 404 if we submit the correct answer again" do
+    response = Net::HTTP.get_response(uri)
+    answer = get_answer(response)
 
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      new_response = http.request_post('/', answer.to_json)
+      expect(new_response.code).to eq("200")
+      expect(new_response.message).to eq("OK")
+    end
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      new_response = http.request_post('/', answer.to_json)
+      expect(new_response.code).to eq("404")
+      expect(new_response.message).to eq("Not Found")
+    end
+  end
+
+  it "sends 400 if incorrect numbers are submitted" do
+    response = Net::HTTP.get_response(uri)
+    answer = get_incorrect_answer(response)
+
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      new_response = http.request_post('/', answer.to_json)
+      expect(new_response.code).to eq("400")
+      expect(new_response.message).to eq("Bad Request")
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,41 @@ ENV['RACK_ENV'] = 'test'
 
 require File.expand_path '../../app', __FILE__
 require 'rack/test'
+require 'json'
 
 module RSpecMixin
   include Rack::Test::Methods
   def app() Sinatra::Application end
 end
 
+module Captcha
+  include Rack::Test::Methods
+  def get_answer(response)
+    body = JSON.parse(response.body)
+    sentence = body["sentence"]
+    exclude = body["exclude"]
+    word_freq = Hash.new(0)
+    words = sentence.split(/\W+/).map(&:downcase)
+    words.each do |word|
+      word_freq[word] += 1 unless exclude.include? word
+    end
+
+    post_data = {
+      "sentence" => sentence,
+      "exclude" => exclude,
+      "answer" => word_freq
+    }
+  end
+
+  def get_incorrect_answer(response)
+    correct = get_answer(response)
+    key = correct["answer"].keys.sample(1)
+    correct["answer"][key] += 1
+    correct
+  end
+end
+
 RSpec.configure { |c| c.include RSpecMixin }
+RSpec.configure do |c|
+  c.include Captcha
+end
